8be2d91623d328bc745dd4a3931776e91095b504
jacllyr <jacllyrthomas@gmail.com>
2023-01-20 21:00:22 +0000

    update folders

---
+++ Week1/BlinkyTest3/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+const int LED_PIN = 12;
+const int DELAY = 500;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("ON");
+  digitalWrite(LED_PIN, HIGH);
+  delay(DELAY);
+  Serial.println("OFF");
+  digitalWrite(LED_PIN, LOW);
+  delay(DELAY);
+}
